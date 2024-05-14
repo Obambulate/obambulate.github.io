573e1f2784654bb77edfb2bf35fc8720502d31c9
Obambulate <dmwesseler@gmail.com>
2024-05-13 20:19:19 -0400

    BALL MOVE CONSTANTLY!

--- circleSimulation.js
+++ circleSimulation.js
@@ -1,8 +1,11 @@
 console.clear();
 ("use strict");
 
-const startRestart = document.createElement('button');
-startRestart.innerHTML = ("Start/Restart");
+const startResetButton = document.createElement('button');
+startResetButton.innerHTML = ("Start");
+const pauseButton = document.createElement('button');
+pauseButton.innerHTML = ("Pause/Unpause");
+
 
 const canvas = document.createElement('canvas');
 canvas.width = "700";
@@ -10,17 +13,20 @@ canvas.height = "700";
 canvas.style = "border:1px solid black";
 
 var body = document.getElementsByTagName("body")[0];
-body.appendChild(startRestart);
+body.appendChild(startResetButton);
+body.appendChild(pauseButton);
 body.appendChild(canvas);
 
 const ctx = canvas.getContext("2d");
 
 let running = 0;
 let start, previousTimeStamp;
-const gravity = {x: 0, y: 0.5}; //Gravity == acceleration / time
-const bounce = 1;
-const velStart = {x: -3, y: 10};
-const posStart = {x: (canvas.width/2), y: 30};
+const bounce = 0.4;
+const speedStart = {x: 5.43252354234, y: 10};
+const speedMultiplier = 2
+const posStart = {x: (canvas.width/2), y: 10};
+
+//future map implementation//
 
 const map1 = {
 	leftWall: 1,
@@ -28,9 +34,11 @@ const map1 = {
 	floor: 1
 }
 
+//properties of the first ball//
+
 const ball1 = {
 	pos: {x: 100, y: 100},
-	vel: {x: velStart.x, y: velStart.y},
+	speed: {x: speedStart.x * speedMultiplier, y: speedStart.y * speedMultiplier},
 	size: 10,
 	shape: "circle",
 	fill: 1,
@@ -38,50 +46,48 @@ const ball1 = {
 	lineWidth: 1,
 }
 
-
 function reset(obj) { 
 	obj.pos.x = posStart.x;
 	obj.pos.y = posStart.y;
-	obj.vel.x = velStart.x;
-	obj.vel.y = velStart.y;
+	obj.speed.x = speedStart.x;
+	obj.speed.y = speedStart.y;
 }
 
 function update(obj, map) {
 
-	obj.vel.x += gravity.x;
-	obj.vel.y += gravity.y;
-	obj.pos.x += obj.vel.x;
-	obj.pos.y += obj.vel.y;
+	obj.pos.x += obj.speed.x;
+	obj.pos.y += obj.speed.y;
 
 	if (obj.pos.y + obj.size >= canvas.height) {
 
 		obj.pos.y = (canvas.height - obj.size);
-        obj.vel.y = -Math.abs(obj.vel.y) * bounce;
+        obj.speed.y = obj.speed.y * -1;
 
 	}
 
 	if (obj.pos.y - obj.size <= 0) {
 
 		obj.pos.y = (0 + obj.size);
-        obj.vel.y = Math.abs(obj.vel.y) * bounce;
+        obj.speed.y = obj.speed.y * -1;
 
 	}
 
 	if (obj.pos.x + obj.size >= canvas.width) {
 
 		obj.pos.x = (canvas.width - obj.size);
-        obj.vel.x = -Math.abs(obj.vel.x) * bounce;
+        obj.speed.x = obj.speed.x * -1;
 
 	}
 
 	if (obj.pos.x - obj.size <= 0) {
 
 		obj.pos.x = (0 + obj.size);
-        obj.vel.x = Math.abs(obj.vel.x) * bounce;
+        obj.speed.x = obj.speed.x * -1;
 
 	}
 
-	//console.log(ball1.pos.x, ball1.pos.y, ball1.vel.x, ball1.vel.y);
+	//console.log(ball1.pos.x, ball1.pos.y, ball1.speed.x, ball1.speed.y);
+
 }
 
 
@@ -114,45 +120,70 @@ function draw(obj) {
 
 
 function step(timeStamp) {
-	if (start === undefined || NaN) {
-	  start = timeStamp;
-	}
+	
+	if (running == 1) {
+		
+		if (start === undefined || NaN) {
+
+	  		start = timeStamp;
+		}
   
-	if (previousTimeStamp !== timeStamp){
+		if (previousTimeStamp !== timeStamp){
 
-		mainLoop();
+			mainLoop();
 
+		}
 	}
 }
 
 //Main loop for this program.
-async function mainLoop() {
+function mainLoop() {
 	ctx.clearRect(0, 0, 950, 950);
 	update(ball1);
 	draw(ball1);
 	window.requestAnimationFrame(step);
 }
 
-function startStop() {
+async function startReset() {
 
 	console.log(running);
 
-	if (running = 1){
+	if (running == 1){
 
-		reset(ball1);
-		console.log("reset");
 		running = 0;
-
-	} 
-	
-	if (running == 0) {
-
-		mainLoop();
+		reset(ball1);
 		running = 1;
+
+		console.log("reset");
+
+	} else if (running == 0) {
+		
+		running = 1;
+		mainLoop();
+		
 		console.log("run");
 
 	}
 
 }
 
-startRestart.addEventListener("click", startStop);
+function pause() {
+
+	switch (running) {
+		case running = 1:
+			running = 0;
+			console.log("reset");
+			break;
+
+		case running = 0:
+			running = 1;
+			mainLoop();
+			break;
+		default:
+			console.log("woops");
+			console.log(running);
+	}
+}
+
+startResetButton.addEventListener("click", startReset);
+pauseButton.addEventListener("click", pause);
